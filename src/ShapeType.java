f9fa3c697f4ec8a6565ea4d501705a1e762c8f5c
Kadir <ercinkadir90@gmail.com>
2016-11-14 19:40:30 +0100

    Rajout de l'enum ShapeType pour distinguer les formes

---
+++ src/ShapeType.java
@@ -0,0 +1,16 @@
+/**
+ * Created by KadirF on 14/11/2016.
+ */
+public enum ShapeType {
+    SQUARE,
+    RECTANGLE,
+    TRIANGLE,
+    STAR,
+    PENTAGON,
+    DIAMOND,
+    CROSS,
+    TEAR,
+    GEM,
+    OVAL;
+}
+
